b5845e844c730499dff67b215010c48ca3042ecf
Artem Mazin <minima1s@yandex.ru>
2023-07-18 20:21:46 +0300

    feat: добавить компонент navigation в header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../images/logo.svg';
+import Navigation from '../Navigation/Navigation';
 import './Header.css';
 
 const Header = () => {
   return (
     <header className='header'>
-      <img
-        src={logo}
-        alt='Логотип проекта: Место'
-        className='header__logo'
-      />
-      <div className='header__menu'>
-        <Link className='header__link'>Регистрация</Link>
-        <button
-          type='button'
-          className='header__button'>
-          Войти
-        </button>
-      </div>
+      <Navigation className='nav' />
     </header>
   );
 };
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.svg';
+import './Navigation.css';
 
 const Navigation = () => {
-  return <div></div>;
+  return (
+    <nav className='nav'>
+      <Link to={'/'}>
+        <img
+          src={logo}
+          alt='Логотип проекта: Место'
+          className='header__logo'
+        />
+      </Link>
+
+      <div className='nav__menu'>
+        <Link
+          to={'/signup'}
+          className='nav__link'>
+          Регистрация
+        </Link>
+        <button
+          type='button'
+          className='nav__button'>
+          Войти
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
