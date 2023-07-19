f62160deaa1ba4a938a3be8886a0240ae3d810e5
Artem Mazin <minima1s@yandex.ru>
2023-07-19 22:55:08 +0300

    feat: сверстать секцию about-me

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -8,14 +8,15 @@ const AboutMe = () => {
     <section className='about-me'>
       <h2 className='section-name'>Студент</h2>
       <div className='about-me__content'>
-        <div className='about-me__description'>
+        <div>
           <h2 className='main-title'>Артем</h2>
           <article className='description about-me__description'>
             <h2 className='description__title about-me__title'>Фронтенд-разработчик, 32 года</h2>
             <p className='description__text about-me__text'>
               Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать
               музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После
-              того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
+              того, как прошёл курс по веб&#8209;разработке, начал заниматься фриланс-заказами и ушёл с постоянной
+              работы.
             </p>
             <Link
               to={'https://github.com/ArtemMazin'}
@@ -28,7 +29,8 @@ const AboutMe = () => {
         <img
           src={avatar}
           alt='Фотография автора'
-          className='about-me__avatar'></img>
+          className='about-me__avatar'
+        />
       </div>
     </section>
   );
