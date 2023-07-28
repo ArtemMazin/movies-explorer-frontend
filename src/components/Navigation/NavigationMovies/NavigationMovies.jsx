e61bcc713428cffc580a458e23f6699895a203b8
ArtemMazin <minima1s@yandex.ru>
2023-07-28 16:54:34 +0300

    feat: сверстать компонент header 768px

--- src/components/Navigation/NavigationMovies/NavigationMovies.jsx
+++ src/components/Navigation/NavigationMovies/NavigationMovies.jsx
@@ -1,33 +1,52 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './NavigationMovies.css';
 
 const NavigationMovies = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  function toggleHideMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <div className='nav-movies'>
-      <ul className='nav-movies__links'>
-        <li>
-          <NavLink
-            to={'/movies'}
-            className='nav-movies__link'>
-            Фильмы
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to={'/saved-movies'}
-            className={({ isActive }) => `nav-movies__link ${isActive ? 'active' : ''}`}>
-            Сохранённые фильмы
-          </NavLink>
-        </li>
-      </ul>
-      <Link
-        to={'/profile'}
-        className='nav-movies__profile-link'>
-        <div className='nav-movies__profile-svg'></div>
-        Аккаунт
-      </Link>
-    </div>
+    <>
+      {!isMenuOpen ? (
+        <button
+          className='header__menu-hamburger'
+          onClick={toggleHideMenu}>
+          <span className='header__menu-bar' />
+        </button>
+      ) : (
+        <button
+          className='header__close-menu-btn'
+          onClick={toggleHideMenu}
+        />
+      )}
+      <div className='nav-movies'>
+        <ul className='nav-movies__links'>
+          <li>
+            <NavLink
+              to={'/movies'}
+              className='nav-movies__link'>
+              Фильмы
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={'/saved-movies'}
+              className={({ isActive }) => `nav-movies__link ${isActive ? 'active' : ''}`}>
+              Сохранённые фильмы
+            </NavLink>
+          </li>
+        </ul>
+        <Link
+          to={'/profile'}
+          className='nav-movies__profile-link'>
+          <div className='nav-movies__profile-svg'></div>
+          Аккаунт
+        </Link>
+      </div>
+    </>
   );
 };
 
