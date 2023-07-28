1d13b9916eb6ff3b1e437f330ee9d4ca3b6b292a
Artem Mazin <minima1s@yandex.ru>
2023-07-29 00:58:37 +0300

    feat: сверстать Movie 768px, сделать хук orientation, добавить фильмы по клику

--- src/components/Movies/MoviesCardList/MoviesCardList.jsx
+++ src/components/Movies/MoviesCardList/MoviesCardList.jsx
@@ -1,29 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import { initialCards, favoriteCards } from '../../../utils/constants';
 import { useLocation } from 'react-router-dom';
+import useScreenOrientation from '../../../hooks/useScreenOrientation';
 
 const MoviesCardList = () => {
+  const [countRenderMovies, setCountRenderMovies] = useState(0);
+  const [countMoreMovies, setCountMoreMovies] = useState(0);
   const location = useLocation();
+  const orientation = useScreenOrientation();
+
+  useEffect(() => {
+    const screenWidth = window.screen.width;
+    if (screenWidth >= 1280) {
+      setCountRenderMovies(16);
+      setCountMoreMovies(4);
+    } else if (screenWidth >= 768 && screenWidth < 1280) {
+      setCountRenderMovies(8);
+      setCountMoreMovies(2);
+    } else if (screenWidth < 768) {
+      setCountRenderMovies(5);
+      setCountMoreMovies(2);
+    }
+  }, [orientation]);
+
+  const getMoreMovies = () => {
+    setCountRenderMovies(countRenderMovies + countMoreMovies);
+  };
 
   return (
     <section className='movies'>
       {location.pathname === '/movies' || location.pathname === '/movies/' ? (
         <>
           <ul className='movies__list'>
-            {initialCards.map((card, i) => (
+            {initialCards.slice(0, countRenderMovies).map((card, i) => (
               <MoviesCard
                 card={card}
                 key={i}
               />
             ))}
           </ul>
-          <button
-            className='movies__button'
-            type='button'>
-            Ещё
-          </button>
+
+          {/* !!!!!!!!!! не забыть заменить >= на > !!!!!!!!!!! */}
+          {initialCards.length >= countRenderMovies && (
+            <button
+              className='movies__button'
+              type='button'
+              onClick={getMoreMovies}>
+              Ещё
+            </button>
+          )}
         </>
       ) : (
         <ul className='movies__list'>
